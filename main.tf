3e2690375f6caf53f7392fb6f295e878dd7eb87d
Priya <lakpry@gmail.com>
2023-03-15 12:38:02 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,116 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.66.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "CloudAcademy"
+    Demo = "Terraform"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "subnet"
+    Type = "Public"
+  }
+}
+
+
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    "Name"  = "Main"
+    "Owner" = "CloudAcademy"
+  }
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "Public"
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+
+resource "aws_security_group" "webserver" {
+  name        = "Webserver"
+  description = "Webserver network traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  ingress {
+    description = "80 from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow traffic"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0df24e148fdb9f1d8"
+   instance_type          = "t3.micro"
+  key_name               = "mykeypair"
+  subnet_id              = aws_subnet.subnet.id
+  vpc_security_group_ids = [aws_security_group.webserver.id]
+  associate_public_ip_address = true
+
+  #userdata
+  user_data = <<EOF
+#!/bin/bash
+sudo yum update
+sudo yum install nginx
+sudo nginx
+EOF
+
+  tags = {
+    Name = "CloudAcademy"
+  }
+}
