bac4e52dd5a3b2d98f29977d6147a1fb3ad7da35
lakpry <58025129+lakpry@users.noreply.github.com>
2023-03-23 18:17:37 -0500

    Add files via upload

---
+++ newmain.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.59.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0df24e148fdb9f1d8"
+   instance_type          = "t3.micro"
+  key_name               = "Mykeypair"
+  associate_public_ip_address = true
+  
+  tags = {
+    Name = "my_vm"
+  }
+}
